4f882b17f6e53f8411ed354b985dee3f3e659909
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-08-01 17:18:35 +0700

    period navigation component layout

--- src/components/PeriodNavigation/PeriodNavigation.tsx
+++ src/components/PeriodNavigation/PeriodNavigation.tsx
@@ -1,4 +1,5 @@
 import cls from './PeriodNavigation.module.scss';
+import LeftIcon from 'shared/assets/icons/left-icon.svg'
 
 interface PeriodNavigationProps {
   currentSlide: number;
@@ -8,24 +9,28 @@ interface PeriodNavigationProps {
   onNext: () => void;
 }
 
-const PeriodNavigation = ({ currentSlide, totalSlides, onPrev, onNext, clasName }: PeriodNavigationProps) => {
+const PeriodNavigation = (props: PeriodNavigationProps) => {
+  const { currentSlide, totalSlides, onPrev, onNext, clasName } = props;
+  const count = currentSlide + 1 < 10 ? `0${currentSlide + 1}` : currentSlide + 1;
+  const total = totalSlides < 10 ? `0${totalSlides}` : totalSlides;
+
   return (
-    <div className={`${cls.periodNavigation} ${clasName}`}>
-      <span>{currentSlide + 1}/{totalSlides}</span>
+    <div className={`${cls.wrapper} ${clasName}`}>
+      <span>{count}/{total}</span>
       <div>
         <button
-          className={cls.navButton}
+          className={cls.button}
           onClick={onPrev}
           disabled={currentSlide === 0}
         >
-          {'<'}
+          <LeftIcon/>
         </button>
         <button
-          className={cls.navButton}
+          className={cls.button}
           onClick={onNext}
           disabled={currentSlide === totalSlides - 1}
         >
-          {'>'}
+          <LeftIcon style={{transform: 'rotate(-180deg)'}} />
         </button>
       </div>
     </div>
